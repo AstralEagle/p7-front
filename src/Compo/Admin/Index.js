cd63fdf86b83a00a1ad4dea5823cb77768329ebe
Arthur Dias <arthur.dias@live.fr>
2022-03-11 19:37:03 +0100

    'preMentora'

--- src/Compo/Admin/Index.js
+++ src/Compo/Admin/Index.js
@@ -10,8 +10,9 @@ import Header from '../../Outil/header'
 
 export default function MessageReport() {
 
+  const [nbrReport,setReport] = useState(2)
+
   const [reportViewer, setView] = useState(0);
-  console.log(reportViewer)
   const callMessage = (e) => {
     setView(0);
   }
@@ -22,6 +23,10 @@ export default function MessageReport() {
     setView(2)
   }
 
+  const onChange = (e) => {
+    setReport(e.target.value)
+    console.log(nbrReport);
+  }
 
   return (
     <div>
@@ -29,8 +34,9 @@ export default function MessageReport() {
           <p onClick={callMessage}>1</p>
           <p onClick={callPost}>2</p>
           <p onClick={callComment}>3</p>
+      <input type="number" name='NbrReport' value={nbrReport} onChange={onChange} />
       </div>
-      {reportViewer === 0 && <ReportMessage/>}
+      {reportViewer === 0 && <ReportMessage />}
       {reportViewer === 1 && <ReportPost/>}
       {reportViewer === 2 && <ReportComment/>}
 
